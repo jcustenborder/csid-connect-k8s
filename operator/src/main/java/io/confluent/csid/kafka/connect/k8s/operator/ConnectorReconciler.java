0a922c4e1e8a27630fe6a2037e96417185888049
Jeremy Custenborder <jcustenborder@gmail.com>
2022-07-29 15:10:50 -0500

    Removed unused code.

--- operator/src/main/java/io/confluent/csid/kafka/connect/k8s/operator/ConnectorReconciler.java
+++ operator/src/main/java/io/confluent/csid/kafka/connect/k8s/operator/ConnectorReconciler.java
@@ -1,8 +1,6 @@
 package io.confluent.csid.kafka.connect.k8s.operator;
 
 import io.confluent.csid.kafka.connect.k8s.KafkaConnector;
-import io.fabric8.kubernetes.api.model.ConfigMap;
-import io.fabric8.kubernetes.api.model.apps.StatefulSet;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
@@ -10,8 +8,6 @@ import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Set;
 
 @ControllerConfiguration(
     dependents = {
@@ -27,10 +23,6 @@ public class ConnectorReconciler implements Reconciler<KafkaConnector> {
   @Override
   public UpdateControl<KafkaConnector> reconcile(KafkaConnector connector, Context<KafkaConnector> context) throws Exception {
     log.info("reconcile() - connector = {}", connector);
-
-    Set<ConfigMap> configMaps = context.getSecondaryResources(ConfigMap.class);
-    Set<StatefulSet> statefulSets = context.getSecondaryResources(StatefulSet.class);
-
     return UpdateControl.noUpdate();
   }
 }
